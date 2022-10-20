21e30d73e67f660509c2d2a27257692717a08246
jacko0032 <59780454+jacko0032@users.noreply.github.com>
2022-10-20 09:46:37 +0100

    Add files via upload

---
+++ Get-ExplicitPermissions.ps1
@@ -0,0 +1,10 @@
+﻿$path = Read-Host "Enter Path"
+Write-Host "Now searching, this could take some time... Please wait."
+
+
+
+$directories = Get-ChildItem $path -Recurse -Directory -ErrorAction SilentlyContinue
+$permissions = $directories | ForEach-Object -Process {Get-Acl $_.FullName}
+$explicit = $permissions | Where-Object {$_.AreAccessRulesProtected -eq $true} | Select Path
+$explicitpath = $explicit.Path | Convert-Path
+Get-ACL $explicitpath | Select-Object Path -ExpandProperty Access | Select Path,FileSystemRights,AccessControlType,IdentityReference | Export-CSV C:\permissions.csv
